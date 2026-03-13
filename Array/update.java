0f08cee3d4111efdbd43da981bd859b6125fbcba
Tushar Patil <tusharpatil2005@example.com>
2026-03-13 17:33:27 +0530

    new

--- Array/update.java
+++ Array/update.java
@@ -19,3 +19,14 @@ public class update {
         System.out.println();
     }
 }
+
+
+// output
+// marks: 96marks: 97marks: 79
+/* Algorithm (Update Array)
+      1. Start
+      2. Take an array of marks
+      3. Traverse the array and update each mark by adding 1
+      4. Print the updated marks
+      5. Stop
+   */
